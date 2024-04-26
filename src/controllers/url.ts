cc9f222396d0f7d383433d4e8555ea01c2703fa3
manuel <macq09876@gmail.com>
2024-04-25 21:52:23 -0500

    controlador y manejo de error

---
+++ src/controllers/url.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { handleHttp } from "../utils/error.handle";
+
+const getUrl = (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    handleHttp(res, 'ERROR_GET_URL')
+  }
+}
+
+const getUrls = (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    handleHttp(res, 'ERROR_GET_URLS')
+  }
+}
+
+const postUrl = (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    handleHttp(res, 'ERROR_POST_URL')
+  }
+}
+
+const deletUrl = (req: Request, res: Response) => {
+  try {
+    
+  } catch (error) {
+    handleHttp(res, 'ERROR_DELETE_URL')
+  }
+}
